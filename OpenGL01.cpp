073948749172d886bc183576f74e96a0433b5916
gooyuan <songfangxi2012@163.com>
2018-08-23 01:16:46 +0800

    熟悉glut 创建窗口 api, opengl primitive draw api

--- OpenGL01.cpp
+++ OpenGL01.cpp
@@ -35,15 +35,41 @@ void DrawMyObjects() {
     glVertex2f(2.5, -1);
     glVertex2f(6.5, 1);
     glVertex2f(4.5, -6.25);
+
+    glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+    glColor3f(1.0f, 0.0f, 0.0f); // Red
+    glVertex2f(-0.5f, -0.5f);    // x, y
+    glVertex2f(0.5f, -0.5f);
+    glVertex2f(0.5f, 0.5f);
+    glVertex2f(-0.5f, 0.5f);
+
     glEnd();
+}
+
+void display() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+    glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+
+    // Draw a Red 1x1 Square centered at origin
+    glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+    glColor3f(1.0f, 0.0f, 0.0f); // Red
+    glVertex2f(-0.5f, -0.5f);    // x, y
+    glVertex2f(0.5f, -0.5f);
+    glVertex2f(0.5f, 0.5f);
+    glVertex2f(-0.5f, 0.5f);
+    glEnd();
+
+    glFlush();  // Render now
 }
 
 void RenderScene() {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0, 1.0, 0.0);
     DrawMyObjects();
+//    display();
     glFlush();
 }
+
 
 int main(int argc, char **argv) {
     render_1(argc, argv);
@@ -59,8 +85,13 @@ void render_1(int argc, char **argv) {
 
     glClearColor(0.51, 0.51, 0.51, 0.5);
 
+    // 可以将函数当作参数传递, 不管是C,C++都是自有的么?
     glutReshapeFunc(ChangeSize);
+
+    // 这里是关键代码
     glutDisplayFunc(RenderScene);
 
+
     glutMainLoop();
+
 }
