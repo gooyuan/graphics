27572e2c030a8543f6b994a43e07759d279918a1
gooyuan <songfangxi2012@163.com>
2018-10-21 23:45:54 +0800

    light create source file

---
+++ src/opengl/Light.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sophimp on 2018/10/21.
+//
+
+#include "Light.h"
---
+++ src/opengl/Light.h
@@ -0,0 +1,14 @@
+//
+// Created by Sophimp on 2018/10/21.
+//
+
+#ifndef CLIONOPENGL_LIGHT_H
+#define CLIONOPENGL_LIGHT_H
+
+
+class Light {
+
+};
+
+
+#endif //CLIONOPENGL_LIGHT_H
